3f4c94cc3b97316a8d6ec9b2bd34c698ecd8c5ce
JohnIanOngayi <ongayijohnian@gmail.com>
2025-01-06 14:40:01 +0300

    Defined Category model
    - Removed launchBrowser directives in launchSettings

---
+++ src/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace src.Models;
+
+public class Category
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    required public string Name { get; set; }
+    public int DisplayOrder { get; set; }
+}
